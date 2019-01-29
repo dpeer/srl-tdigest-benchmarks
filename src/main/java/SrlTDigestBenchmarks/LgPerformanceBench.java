4e15227c5eca738e8324fab05c7c5f61cb775d03
David Peer <david.peer@microfocus.com>
2019-01-29 19:48:48 +0200

    Added multi threading - not working

--- src/main/java/SrlTDigestBenchmarks/LgPerformanceBench.java
+++ src/main/java/SrlTDigestBenchmarks/LgPerformanceBench.java
@@ -4,10 +4,58 @@ import com.tdunning.math.stats.MergingDigest;
 import java.nio.ByteBuffer;
 import java.util.Random;
 
+class TDigestAgg extends Thread {
+    private Thread t;
+    private String threadName;
+    private MergingDigest[] aggregatorTDigests;
+    private ByteBuffer[][] lgsBuffers;
+    private ByteBuffer[] aggsByteBuffers;
+    private int start;
+    private int end;
+
+    TDigestAgg(String name, MergingDigest[] aggregatorTDigests, ByteBuffer[][] lgsBuffers, ByteBuffer[] aggsByteBuffers, int start, int end) {
+        threadName = name;
+        this.aggregatorTDigests = aggregatorTDigests;
+        this.lgsBuffers = lgsBuffers;
+        this.aggsByteBuffers = aggsByteBuffers;
+        this.start = start;
+        this.end = end;
+
+        for (int i = this.start; i < this.end; i++) {
+            this.aggregatorTDigests[i] = new MergingDigest(SrlConsts.TdAggCompression);
+        }
+    }
+
+    public void run() {
+        for (int i = start; i < end; i++) {
+            for (int j = 0; j < SrlConsts.MaxDimensionsPerLg; j++) {
+                aggregatorTDigests[((i % SrlConsts.MaxRegions) * SrlConsts.MaxDimensionsPerLg) + j].add(MergingDigest.fromBytes(lgsBuffers[i][j]));
+            }
+        }
+
+        for (int i = start; i < end; i++) {
+            int bufSize = aggregatorTDigests[i].smallByteSize();
+            aggsByteBuffers[i] = ByteBuffer.allocate(bufSize);
+            aggregatorTDigests[i].asSmallBytes(aggsByteBuffers[i]);
+        }
+
+        System.out.println("Thread " +  threadName + " exiting.");
+    }
+
+    public void start() {
+        System.out.println("Starting " +  threadName);
+        if (t == null) {
+            t = new Thread(this, threadName);
+            t.start();
+        }
+    }
+}
+
 public class LgPerformanceBench {
     private static Random gen = new Random();
     private static long startDur;
     private static long endDur;
+    private static final int threadsCount = 50;
 
     public static double[][] generateLgData() {
         // Create random data
@@ -45,7 +93,6 @@ public class LgPerformanceBench {
         return lgsByteBuffers;
     }
 
-    // Todo: Split processing to different threads
     public static ByteBuffer[] mergeLgsTDigests(ByteBuffer[][] lgsBuffers) {
         System.out.println("mergeLgsTDigests: Merging " + lgsBuffers.length + " TDigests and converting to buffers ");
         startDur = System.currentTimeMillis();
@@ -77,6 +124,29 @@ public class LgPerformanceBench {
         return aggsByteBuffers;
     }
 
+    public static ByteBuffer[] mergeLgsTDigestsMultiT(ByteBuffer[][] lgsBuffers) throws InterruptedException {
+        System.out.println("mergeLgsTDigestsMultiT: Merging " + lgsBuffers.length + " TDigests and converting to buffers ");
+        startDur = System.currentTimeMillis();
+
+        var aggregatorTDigests = new MergingDigest[SrlConsts.MaxDimensions];
+        var aggsByteBuffers = new ByteBuffer[SrlConsts.MaxDimensions];
+        var threads = new Thread[threadsCount];
+
+        for (int i = 0; i < threadsCount; i++) {
+            threads[i] = new TDigestAgg("Thread " + i, aggregatorTDigests, lgsBuffers, aggsByteBuffers, SrlConsts.MaxDimensions / threadsCount * i, SrlConsts.MaxDimensions / threadsCount * (i + 1));
+            threads[i].start();
+        }
+
+        for (int i = 0; i < threadsCount; i++) {
+            threads[i].join();
+        }
+
+        endDur = System.currentTimeMillis();
+        System.out.println("mergeLgsTDigestsMultiT: duration (msec) = " + ((endDur - startDur)));
+
+        return aggsByteBuffers;
+    }
+
     public static void main(String[] args) {
 
 //        var data = generateLgData();
@@ -103,6 +173,12 @@ public class LgPerformanceBench {
         }
 
         mergeLgsTDigests(lgsBuffers);
+
+        try {
+            mergeLgsTDigestsMultiT(lgsBuffers);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         // todo: Add processing of all Aggs and reporter
     }
